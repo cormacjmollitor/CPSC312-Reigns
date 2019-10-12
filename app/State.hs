d568c4b38db1c48fc29ef292f3d4642f57bb1268
Matthew Ridderikohff <matthewridderikhoff@gmail.com>
2019-10-12 16:14:49 -0700

    rotates card with key presses, updates week if a card direction is selected

--- app/State.hs
+++ app/State.hs
@@ -8,16 +8,26 @@ initialCard :: Card
 initialCard = ("left or right, press again to confirm", (0,0,0,0), (0,0,0,0))
 
 initialState :: State
-initialState = (None, initialCard, (20,20,20,20), 0)
+initialState = (Types.None, initialCard, (20,20,20,20), 0)
 
 deck :: [Card]
 deck = [
   ("A", (0,-1,0,1), (0,1,0,-1)),
   ("B", (3,-4,0,1), (0,6,0,0))]
 
--- todo: actually setup the key actions
--- Currently only changes cards upon left-arrow
 inputHandler :: Event -> State -> State
-inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) state = state
-inputHandler (EventKey (SpecialKey KeyRight) Down _ _) state = state
+inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) state = handleLeftKey state
+inputHandler (EventKey (SpecialKey KeyRight) Down _ _) state = handleRightKey state
 inputHandler _ s = s
+
+-- todo: actually make changes beyond updating CurrentKey
+handleLeftKey :: State -> State
+handleLeftKey (Types.Left, card, resources, week) = (Types.None, card, resources, week + 1)
+handleLeftKey (Types.Right, card, resources, week) = (Types.None, card, resources, week)
+handleLeftKey (Types.None, card, resources, week) = (Types.Left, card, resources, week)
+
+-- todo: actually make changes beyond updating CurrentKey
+handleRightKey :: State -> State
+handleRightKey (Types.Right, card, resources, week) = (Types.None, card, resources, week + 1)
+handleRightKey (Types.Left, card, resources, week) = (Types.None, card, resources, week)
+handleRightKey (Types.None, card, resources, week) = (Types.Right, card, resources, week)
--- app/UI.hs
+++ app/UI.hs
@@ -3,17 +3,30 @@ module UI (drawState) where
 import Graphics.Gloss
 import Types
 
-getText :: String -> Picture
-getText s = Translate (-350) (200) -- shift the text to the middle of the window
-  $ Scale 0.25 0.25 -- display it half the original size
-  $ Text s
-
-getCard :: CurrentKey -> Picture
-getCard card = Translate (1) (-100) -- shift the card
+cardDrawing = Translate (0) (-100) -- shift the card into the middle
   $ Scale 0.5 0.5 -- display it half the original size
   $ rectangleWire 400 600
+
+drawWeekText :: Int -> Picture
+drawWeekText num = Translate (-100) (300) -- shift the start of the text to the left of the window
+  $ Scale 0.25 0.25
+  $ Text ("Week: "++show num)
+
+drawCardText :: String -> Picture
+drawCardText s = Translate (-350) (100) -- shift the start of the text to the left of the window
+  $ Scale 0.25 0.25
+  $ Text s
+
+drawCard :: CurrentKey -> Picture
+drawCard None = cardDrawing
+drawCard Types.Left = Translate (-80) (0)
+  $ Rotate (-12)
+  $ cardDrawing
+drawCard Types.Right = Translate (80) (0)
+  $ Rotate (12)
+  $ cardDrawing
 
 -- todo: complete so it actually updates based on the resource values, turns the card, shows the resource symbols
 -- note: don't worry too much about the text looking perfect, i think it would be too hard to really dig into
 drawState :: State -> Picture 
-drawState (currentKey, (text, _, _), _, _) = Pictures [getText text, getCard currentKey]
+drawState (currentKey, (text, _, _), _, week) = Pictures [drawCardText text, drawCard currentKey, drawWeekText week]
