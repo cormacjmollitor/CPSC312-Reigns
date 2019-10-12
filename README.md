0cf982a291af6a8f69ac1b11d46c952dafb331d4
Matthew Ridderikohff <matthewridderikhoff@gmail.com>
2019-10-12 15:30:34 -0700

    Add skeleton of game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CPSC312-Reigns
+to complile: stack build
+to run: stack exec CPSC312-Reigns-exe
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,7 @@ module Lib
     ( someFunc
     ) where
 
+import Graphics.Gloss
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "game starting"
