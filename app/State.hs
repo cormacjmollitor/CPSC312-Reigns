cf5b710f41755b39467de0c52421f070ba7f7339
Cormac Mollitor <cormacjmollitor@gmail.com>
2019-10-16 15:49:56 -0700

    Got rid of some todo comments

--- app/State.hs
+++ app/State.hs
@@ -17,13 +17,11 @@ inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) state = handleLeftKey stat
 inputHandler (EventKey (SpecialKey KeyRight) Down _ _) state = handleRightKey state
 inputHandler _ s = s
 
--- todo: actually make changes beyond updating CurrentKey
 handleLeftKey :: State -> State
 handleLeftKey (Types.Left, card, cardIndex, resources, week) = processMove (Types.Left, card, cardIndex, resources, week) -- This will call the backend
 handleLeftKey (Types.Right, card, cardIndex, resources, week) = (Types.None, card, cardIndex, resources, week) -- following 2 will not call backend
 handleLeftKey (Types.None, card, cardIndex, resources, week) = (Types.Left, card, cardIndex, resources, week)
 
--- todo: actually make changes beyond updating CurrentKey
 handleRightKey :: State -> State
 handleRightKey (Types.Right, card, cardIndex, resources, week) = processMove (Types.Right, card, cardIndex, resources, week) -- This will call the backend
 handleRightKey (Types.Left, card, cardIndex, resources, week) = (Types.None, card, cardIndex, resources, week)
