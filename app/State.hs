c658265e9daff08864a6188ed91efe02d0a83c17
Matthew Ridderikohff <matthewridderikhoff@gmail.com>
2019-10-17 16:31:53 -0700

    Add test cases, comment up State

--- app/State.hs
+++ app/State.hs
@@ -9,7 +9,9 @@ initialResources :: Resources
 initialResources = (15,15,15,15)
 
 initialState :: State
-initialState = (Types.None, initialCard, initialResources, 0)
+initialState = (Types.None, initialCard, initialResources, 0) -- base game
+-- initialState = (Types.None, initialCard, (1,1,1,1), 12) -- almost lost
+-- initialState = (Types.None, initialCard, (20,20,20,20), 29) -- almost lost
 
 inputHandler :: Event -> State -> IO State
 inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) state = handleLeftKey state
@@ -17,11 +19,11 @@ inputHandler (EventKey (SpecialKey KeyRight) Down _ _) state = handleRightKey st
 inputHandler _ s = do return s
 
 handleLeftKey :: State -> IO State
-handleLeftKey (Types.Left, card, resources, week) = processMove (Types.Left, card, resources, week) -- This will call the backend
-handleLeftKey (Types.Right, card, resources, week) = do return (Types.None, card, resources, week) -- following 2 will not call backend
-handleLeftKey (Types.None, card, resources, week) = do return (Types.Left, card, resources, week)
+handleLeftKey (Types.Left, card, resources, week) = processMove (Types.Left, card, resources, week) -- No action
+handleLeftKey (Types.Right, card, resources, week) = do return (Types.None, card, resources, week) -- update CurrentKey
+handleLeftKey (Types.None, card, resources, week) = do return (Types.Left, card, resources, week) -- update CurrentKey
 
 handleRightKey :: State -> IO State
-handleRightKey (Types.Right, card, resources, week) = processMove (Types.Right, card, resources, week) -- This will call the backend
-handleRightKey (Types.Left, card, resources, week) = do return (Types.None, card, resources, week)
-handleRightKey (Types.None, card, resources, week) = do return (Types.Right, card, resources, week)
+handleRightKey (Types.Right, card, resources, week) = processMove (Types.Right, card, resources, week) -- Yes action
+handleRightKey (Types.Left, card, resources, week) = do return (Types.None, card, resources, week) -- update CurrentKey
+handleRightKey (Types.None, card, resources, week) = do return (Types.Right, card, resources, week) -- update CurrentKey
