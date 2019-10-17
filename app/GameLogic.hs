daefffa7e601b0e219b42f9c48a5e6682dd6e372
Cormac Mollitor <cormacjmollitor@gmail.com>
2019-10-17 14:07:46 -0700

    Got it workign with IO. Cards are now random.

--- app/GameLogic.hs
+++ app/GameLogic.hs
@@ -1,18 +1,17 @@
 module GameLogic (deck, processMove, initialCard) where
   import Types
+  import System.Random
 
   deck :: [Card]
   deck = [
     ("A", (0,-1,0,1), (0,1,0,-1)),
     ("B", (3,-4,0,1), (0,6,0,0)),
     ("C", (-7,-1,4,1), (5,-5,0,0)),
-    ("B", (3,-4,0,1), (0,6,0,0)),
-    ("A", (0,-1,0,1), (0,1,0,-1)),
-    ("B", (3,-4,0,1), (0,6,0,0)),
-    ("A", (0,-1,0,1), (0,1,0,-1)),
-    ("B", (3,-4,0,1), (0,6,0,0))]
-
-  deckIncrement = 3 
+    ("D", (3,-4,0,1), (0,6,0,0)),
+    ("E", (0,-1,0,1), (0,1,0,-1)),
+    ("F", (3,-4,0,1), (0,6,0,0)),
+    ("G", (0,-1,0,1), (0,1,0,-1)),
+    ("H", (3,-4,0,1), (0,6,0,0))]
 
   initialCard :: Card
   initialCard = ("left or right, press again to confirm", (0,0,0,0), (0,0,0,0))
@@ -21,31 +20,38 @@ module GameLogic (deck, processMove, initialCard) where
   endGameCard :: Card
   endGameCard = ("Game Over. Press right to play again.", (0,0,0,0), (0,0,0,0))
   
-  processMove :: State -> State
+  processMove :: State -> IO State
   processMove state =
-      let (key, card, cardIndex, resources, week) = state in
-        let (text, leftAction, rightAction) = card in
-          if key == Types.Left
-            then
-              if (text == "Game Over. Press right to play again.")
-                then (Types.None, endGameCard, cardIndex, resources, week) -- TODO: implement quitting maybe?
-              else if (lostGame (updateResources leftAction resources) || (wonGame week))
-                then (Types.None, endGameCard, cardIndex, resources, week) -- Game over
-                else (Types.None, (getNextCard cardIndex), cardIndex + deckIncrement, (updateResources leftAction resources), week + 1)
-          else if key == Types.Right
-            then
-              if (text == "Game Over. Press right to play again.")
-                then (Types.None, initialCard, cardIndex + deckIncrement, (20,20,20,20), 0) -- Initial state but with next card instead
-              else if (lostGame (updateResources rightAction resources) || (wonGame week))
-                then (Types.None, endGameCard, cardIndex, resources, week) -- Game over
-                else (Types.None, (getNextCard cardIndex), cardIndex + deckIncrement, (updateResources rightAction resources), week + 1)
-          else 
-            if (wonGame week) 
-              then (Types.None, endGameCard, cardIndex, resources, week) -- Game over
-              else state -- No move made
+    do
+      let (key, card, resources, week) = state
+      let (text, leftAction, rightAction) = card
+      nextCard <- getNextCard
+      if key == Types.Left
+        then
+          if (text == "Game Over. Press right to play again.")
+            then return (Types.None, endGameCard, resources, week) -- TODO: implement quitting maybe?
+          else if (lostGame (updateResources leftAction resources) || (wonGame week))
+            then  return (Types.None, endGameCard, resources, week) -- Game over
+            else  return (Types.None, nextCard, (updateResources leftAction resources), week + 1)
+      else if key == Types.Right
+        then
+          if (text == "Game Over. Press right to play again.")
+            then return (Types.None, initialCard, (20,20,20,20), 0) -- Initial state but with next card instead
+          else if (lostGame (updateResources rightAction resources) || (wonGame week))
+            then return (Types.None, endGameCard, resources, week) -- Game over
+            else return (Types.None, nextCard, (updateResources rightAction resources), week + 1)
+      else 
+        if (wonGame week) 
+          then return (Types.None, endGameCard, resources, week) -- Game over
+          else return state -- No move made
 
-  getNextCard :: Int -> Card
-  getNextCard index = deck !! ((index + deckIncrement) `mod` length(deck))
+  -- Gets a random card from the deck
+  getNextCard :: IO Card
+  getNextCard =
+    do
+      index <- randomRIO(0, length(deck) - 1) :: IO Int
+      return $ deck !! index
+    
 
   -- Adds the amount of resources in the action to the resources passed in
   updateResources :: Action -> Resources -> Resources
